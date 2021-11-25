e2ddc24d304b3b7a19aa0f500a81aef2b80ef4cd
DevDastider <sayantangd7@gmail.com>
2021-11-25 21:21:19 +0530

    Data Binding enabled

--- app/src/main/java/com/self/project/memeshare/MainActivity.kt
+++ app/src/main/java/com/self/project/memeshare/MainActivity.kt
@@ -2,12 +2,12 @@ package com.self.project.memeshare
 
 import android.content.Intent
 import android.graphics.drawable.Drawable
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
-import android.widget.ProgressBar
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import com.android.volley.Request
 import com.android.volley.toolbox.JsonObjectRequest
 import com.bumptech.glide.Glide
@@ -15,22 +15,36 @@ import com.bumptech.glide.load.DataSource
 import com.bumptech.glide.load.engine.GlideException
 import com.bumptech.glide.request.RequestListener
 import com.bumptech.glide.request.target.Target
+import com.self.project.memeshare.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
     private var currentImageUrl: String?= null
+    private lateinit var binding: ActivityMainBinding
+    private val url="https://meme-api.herokuapp.com/gimme"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding= DataBindingUtil.setContentView(this, R.layout.activity_main)
+        //To load the meme
         loadMeme()
+
+        binding.nextButton.setOnClickListener {
+            loadMeme()
+        }
+
+        binding.shareButton.setOnClickListener {
+            val intent= Intent(Intent.ACTION_SEND)
+            intent.type= "text/plain"
+            intent.putExtra(Intent.EXTRA_TEXT, "Hey, Checkout this cool meme... $currentImageUrl")
+            val chooser= Intent.createChooser(intent, "Share this meme using...")
+            startActivity(chooser)
+        }
     }
 
     private fun loadMeme(){
-        val progressBar = findViewById<ProgressBar>(R.id.progressBar)
-        val url="https://meme-api.herokuapp.com/gimme"
 
-        progressBar.visibility = View.VISIBLE
+        binding.progressBar.visibility = View.VISIBLE
 
         val jsonObjectRequest = JsonObjectRequest(
             Request.Method.GET, url, null,
@@ -44,7 +58,7 @@ class MainActivity : AppCompatActivity() {
                         target: Target<Drawable>?,
                         isFirstResource: Boolean
                     ): Boolean {
-                        progressBar.visibility= View.GONE
+                        binding.progressBar.visibility= View.GONE
                         Log.e("Error", e?.localizedMessage!!)
                         return false
                     }
@@ -56,10 +70,10 @@ class MainActivity : AppCompatActivity() {
                         dataSource: DataSource?,
                         isFirstResource: Boolean
                     ): Boolean {
-                        progressBar.visibility= View.GONE
+                        binding.progressBar.visibility= View.GONE
                         return false
                     }
-                }).into(findViewById(R.id.memeimageview))
+                }).into(binding.memeImageView)
             },
             { error ->
                 Toast.makeText(this, "Something went wrong", Toast.LENGTH_LONG).show()
@@ -68,17 +82,5 @@ class MainActivity : AppCompatActivity() {
         )
 
         MySingleton.getInstance(this).addToRequestQueue(jsonObjectRequest)
-    }
-
-    fun nextMeme(view:View) {
-        loadMeme()
-    }
-
-    fun shareMeme(view: View) {
-        val intent= Intent(Intent.ACTION_SEND)
-        intent.type= "text/plain"
-        intent.putExtra(Intent.EXTRA_TEXT, "Hey, Chckout this cool meme... $currentImageUrl")
-        val chooser= Intent.createChooser(intent, "Share this meme using...")
-        startActivity(chooser)
     }
 }
